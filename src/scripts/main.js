2bc5b3708a9c8efeee006d94d97dcf75118d5bd3
Sukhanov Stas <stas.sukhanov@gmail.com>
2016-01-30 16:09:33 +0100

    Initial dist subtree commit

--- src/scripts/main.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var view = require("./ui/View.jsx"),
-  app = require('./App');
-
-view.render($("#view"));
-
-app.start();
--- src/scripts/model/projects.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var fn = {};
-
-fn.transform = function (transport, opts) {
-  return transport.projects;
-};
-
-module.exports = fn;
